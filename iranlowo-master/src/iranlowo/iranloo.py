85cb3480deae44b2d348ad76a9b10183f8793937
tobi o <tobystic@gmail.com>
2020-02-16 13:59:24 -0800

    Update iranloo.py

--- iranlowo-master/src/iranlowo/iranloo.py
+++ iranlowo-master/src/iranlowo/iranloo.py
@@ -1,3 +1,5 @@
 import iranlowo.adr as ranti
 
 ranti.diacritize_text("ni tootoo ojo gbogbo ni ti ole")
+
+## bear in mind, accents occupy char spaces in the windows terminal as individual characters. This is common for UTF-8 and UTF-16 sequences of code units for NFC chars
